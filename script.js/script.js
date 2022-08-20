56b047f175529b023dc91d6655d41212477b8f4c
Drew Anello <drew.anello@gmail.com>
2022-08-19 22:04:52 -0400

    declaired variables

--- script.js/script.js
+++ script.js/script.js
@@ -1,4 +1,6 @@
-// Var
+// Var's
+const URL = 'https://owen-wilson-wow-api.herokuapp.com/wows/directors';
+const $form = $('form');
+const $input = $('input[type="text"]')
 
-const URL ='https://owen-wilson-wow-api.herokuapp.com/wows/directors';
-
+// 
