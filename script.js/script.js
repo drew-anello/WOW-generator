a3fa0bf09a7ca2158f294a0f2662799581c87b80
Drew Anello <drew.anello@gmail.com>
2022-08-19 22:28:42 -0400

    added event listener

--- script.js/script.js
+++ script.js/script.js
@@ -1,6 +1,15 @@
 // Var's
 const URL = 'https://owen-wilson-wow-api.herokuapp.com/wows/directors';
+console.log(URL)
 const $form = $('form');
 const $input = $('input[type="text"]')
 
-// 
+// Event Listener (submit button)
+
+$form.on('submit', handleGetData)
+
+
+
+
+
+
