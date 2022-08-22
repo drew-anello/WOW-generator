8539d5f7dd425b2383c961032e03dfe9d2dd7d7a
Drew Anello <drew.anello@gmail.com>
2022-08-22 08:42:14 -0400

    added a new js file. used jquery i have for notes

---
+++ script.js/script.bak.js
@@ -0,0 +1,47 @@
+// Var's
+// const URL = 'https://owen-wilson-wow-api.herokuapp.com/wows/random?results=10&sort=movie&direction=desc';
+// console.log(URL)
+// const $form = $('form');
+// let $input = $('input[type="text"]')
+// let $year = $('#year');
+// let $numberOfWows = $('#wowInMovie')
+// let $wowCount = $('#numberWow')
+
+// // Event Listener (submit button)
+
+// $form.on('submit', handleGetData)
+
+// // Functions 
+// function handleGetData(event) {
+//     event.preventDefault()
+//     userInput = $input.val()
+//     if (userInput === '') return;
+
+
+//     $.ajax(URL + userInput).then(function (data) {
+
+//         $('main').append('')
+//         $title.text(data.Title)
+//         $year.text(data.Year)
+//         $numberOfWows.text(data.totalWowsInMovie)
+//         $wowCount.text(data.currentWowinMovie)
+//         $('main').append(`<video src="${data.video}" alt="${data.audio}" controls/>`)
+//         $input.val('')
+
+//     })
+
+// }
+
+// updating using JS
+
+const options = { method: 'GET', headers: { Accept: 'application/json' } };
+
+fetch('https://owen-wilson-wow-api.herokuapp.com/wows/random', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+    
+
+
+
