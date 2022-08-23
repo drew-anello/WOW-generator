04fa1d0d288d8d9f5d3212b520075a54edc067eb
Drew Anello <drew.anello@gmail.com>
2022-08-22 21:16:10 -0400

    ads wow var and eventListener

--- script.js/script.js
+++ script.js/script.js
@@ -1,18 +1,29 @@
 // fetch api///
 ///////////////
-fetch('https://owen-wilson-wow-api.herokuapp.com/wows/random');
+const options = { method: 'GET', headers: { Accept: 'application/json' } };
+fetch('https://owen-wilson-wow-api.herokuapp.com/wows/random', options)
 
+    .then((response) => {
+        const data = response.json()
+})
+  .then((data) => console.log(data))
+    .catch(err => console.error(err));
 // user submit form /
 ///////////////////////
 
 const year = document.getElementsByClassName('number');
+const form = document.getElementById('form');
+const slider = document.getElementById('wowSlide');
+const wow = document.querySelector('.submit')
+// addEventListener('submit', function)
 
-addEventListener('submit')
-const form = document.getElementById('form')
+wow.addEventListener('click', function () {
+    
+})
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-})
+});
 
 
 // Sorting by year
