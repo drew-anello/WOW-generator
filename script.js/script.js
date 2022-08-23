9b05e083f1ce5b8485c30993b089f5e8ca3aaaa7
Drew Anello <drew.anello@gmail.com>
2022-08-22 22:16:55 -0400

    Remove sliding scale ad new api

--- script.js/script.js
+++ script.js/script.js
@@ -1,29 +1,35 @@
 // fetch api///
 ///////////////
-const options = { method: 'GET', headers: { Accept: 'application/json' } };
-fetch('https://owen-wilson-wow-api.herokuapp.com/wows/random', options)
+let data
 
-    .then((response) => {
-        const data = response.json()
-})
-  .then((data) => console.log(data))
-    .catch(err => console.error(err));
+const options = { method: 'GET', headers: { Accept: 'application/json' } };
+fetch('https://owen-wilson-wow-api.herokuapp.com/wows/ordered/0-90', options)
+    .then(response => response.json())
+    .then(response => {
+        data = response
+    })
+    .catch (err => console.error(err));
 // user submit form /
 ///////////////////////
 
+console.log(data)
 const year = document.getElementsByClassName('number');
 const form = document.getElementById('form');
 const slider = document.getElementById('wowSlide');
 const wow = document.querySelector('.submit')
 // addEventListener('submit', function)
 
+// function getYear [2011] {
+// console.log([2011])
+// }
 wow.addEventListener('click', function () {
-    
+
 })
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 });
+
 
 
 // Sorting by year
