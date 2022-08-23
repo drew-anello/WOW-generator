8062b1fe013d063423a899db478d08a7043af08c
krogank9 <wifi.mouse.app@gmail.com>
2022-08-22 23:26:11 -0400

    Update script.js

--- script.js/script.js
+++ script.js/script.js
@@ -7,12 +7,12 @@ fetch('https://owen-wilson-wow-api.herokuapp.com/wows/ordered/0-90', options)
     .then(response => response.json())
     .then(response => {
         data = response
+        console.log(data)
     })
     .catch (err => console.error(err));
 // user submit form /
 ///////////////////////
 
-console.log(data)
 const year = document.getElementsByClassName('number');
 const form = document.getElementById('form');
 const slider = document.getElementById('wowSlide');
@@ -23,7 +23,13 @@ const wow = document.querySelector('.submit')
 // console.log([2011])
 // }
 wow.addEventListener('click', function () {
-
+    let numberOfWowsCount = 0;
+    let yearToMatch = parseInt(document.getElementById("wowYearInput").value);
+    data.forEach(function(movie) {
+        if(movie.year === yearToMatch)
+            numberOfWowsCount += movie.total_wows_in_movie;
+    })
+    document.getElementsByClassName("numOfWow")[0].innerHTML = `Number of Wows: ${numberOfWowsCount}`;
 })
 
 form.addEventListener('submit', (e) => {
