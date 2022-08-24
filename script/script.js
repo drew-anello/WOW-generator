17ffe6ebc9405430467f13fcc6f0511320fc94e6
Drew Anello <drew.anello@gmail.com>
2022-08-24 11:47:31 -0400

    slideEl works

--- script/script.js
+++ script/script.js
@@ -1,12 +1,18 @@
 // Vars
 const formEl = document.querySelector('form');
+const yearEl = document.querySelector('.number');
+const sliderEl = document.querySelector('#wow-slide');
 
 
 
-// user submit form stop pge from refresh 
+// user submit form
 
 formEl.addEventListener('submit', async (e) => {
     e.preventDefault();
 
+    const year = parseInt(yearEl.value)
+    const wowCount = parseInt(sliderEl.value);
+    console.log(sliderEl);
+    
 })
 
