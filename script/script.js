f6416632a0731f54326fa43bf5e66754da4a06c5
Drew Anello <drew.anello@gmail.com>
2022-08-24 11:09:09 -0400

    adds eventlistener to submit prevent page refresh

--- script/script.js
+++ script/script.js
@@ -1,6 +1,12 @@
 // Vars
+const formEl = document.querySelector('form');
+
 
 
 // user submit form stop pge from refresh 
 
+formEl.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+})
 
