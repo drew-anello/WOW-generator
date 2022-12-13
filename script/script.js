71a6b9d5d4f25ede2d06dcc0446b662341909c1c
Drew Anello <drew.anello@gmail.com>
2022-12-13 13:03:15 -0500

    adds import export

--- script/script.js
+++ script/script.js
@@ -1,35 +1,32 @@
+import { movie, year, video } from './wows'
 // Vars
-const formEl = document.querySelector('form');
-const yearEl = document.querySelector('.number');
-const sliderEl = document.querySelector('#wow-slide');
-const outputEl = document.querySelector('#output');
+const formEl = document.querySelector('form')
+const yearEl = document.querySelector('.number')
+const sliderEl = document.querySelector('#wow-slide')
+const outputEl = document.querySelector('#output')
 
-// import {
-//     url
-// } from "./wows"
+formEl.addEventListener('submit', async e => {
+  e.preventDefault()
 
-// user submit form
+  const year = parseInt(yearEl.value)
+  const wowCount = parseInt(sliderEl.value)
 
-formEl.addEventListener('submit', async (e) => {
-    e.preventDefault();
+  // connecting api
+  const url = `https://owen-wilson-wow-api.herokuapp.com/wows/random?results=100&year=${year}&wow_in_movie=${wowCount}`
 
-    const year = parseInt(yearEl.value);
-    const wowCount = parseInt(sliderEl.value);
+  const response = await fetch(url)
+  const movies = await response.json()
 
-    // connecting api
-    const url = `https://owen-wilson-wow-api.herokuapp.com/wows/random?results=100&year=${year}&wow_in_movie=${wowCount}`;
-
-    
-    const response = await fetch(url);
-    const movies = await response.json();
-
-    // appear on page
-    const html = movies.map((movie) => ` 
+  // appear on page
+  const html = movies
+    .map(
+      movie => ` 
     <li>
         <span class="movie-title">${movie.movie}</span>
         <video src="${movie.video['1080p']}" controls></video>
         </li>
-    `).join('');
-    outputEl.innerHTML = html;
-});
-
+    `
+    )
+    .join('')
+  outputEl.innerHTML = html
+})
