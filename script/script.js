d46262a2d8fe983b3989e286c44507ccdc45f085
Drew Anello <drew.anello@gmail.com>
2023-01-26 11:34:48 -0500

    updates api link

--- script/script.js
+++ script/script.js
@@ -1,33 +1,33 @@
 // Vars
-const formEl = document.querySelector('form');
-const yearEl = document.querySelector('.number');
-const sliderEl = document.querySelector('#wow-slide');
-const outputEl = document.querySelector('#output');
-
-
+const formEl = document.querySelector('form')
+const yearEl = document.querySelector('.number')
+const sliderEl = document.querySelector('#wow-slide')
+const outputEl = document.querySelector('#output')
 
 // user submit form
 
-formEl.addEventListener('submit', async (e) => {
-    e.preventDefault();
+formEl.addEventListener('submit', async e => {
+  e.preventDefault()
 
-    const year = parseInt(yearEl.value);
-    const wowCount = parseInt(sliderEl.value);
+  const year = parseInt(yearEl.value)
+  const wowCount = parseInt(sliderEl.value)
 
-    // connecting api
-    const url = `https://owen-wilson-wow-api.herokuapp.com/wows/random?results=100&year=${year}&wow_in_movie=${wowCount}`;
+  // connecting api
+  const url = `https://owen-wilson-wow-api.onrender.com/wows/random?results=100&year=${year}&wow_in_movie=${wowCount}`
 
-    
-    const response = await fetch(url);
-    const movies = await response.json();
+  const response = await fetch(url)
+  const movies = await response.json()
 
-    // appear on page
-    const html = movies.map((movie) => ` 
+  // appear on page
+  const html = movies
+    .map(
+      movie => ` 
     <li>
         <span class="movie-title">${movie.movie}</span>
         <video src="${movie.video['1080p']}" controls></video>
         </li>
-    `).join('');
-    outputEl.innerHTML = html;
-});
-
+    `
+    )
+    .join('')
+  outputEl.innerHTML = html
+})
